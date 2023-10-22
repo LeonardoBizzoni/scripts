6348b0688abccdcd3cca169709a60b4eb76b8df6
LeonardoBizzoni <leo2002714@gmail.com>
2023-10-22 11:42:36 +0200

    caso "==" per esercizi funzione eulero e controllo su input

--- euler_function.c
+++ euler_function.c
@@ -16,10 +16,38 @@ int phi(int n) {
 }
 
 int main(int argc, char **argv) {
+  if (argc < 2) {
+    fprintf(stderr, "Utilizzo: /percorso/al/file/eseguibile numero\n");
+    exit(EXIT_FAILURE);
+  }
+
   int n = atoi(argv[1]);
-  char all_phi = 'y';
+  char all_phi;
+
+  if (!n) {
+    fprintf(stderr, "Non hai inserito un numero.\n");
+    exit(EXIT_FAILURE);
+  }
 
   printf("phi(%d) = %d\n", n, phi(n));
+
+  printf("Calcolare phi(n) == %d? [Y/n] ", n);
+  scanf("%c", &all_phi);
+
+  if (all_phi == 'y' || all_phi == '\n') {
+    printf("n = ");
+
+    for (int i = 1, exit_cond = 0; exit_cond < 1000; i++) {
+      if (phi(i) == n) {
+        printf("%d ", i);
+        exit_cond = 0;
+      } else {
+        exit_cond++;
+      }
+    }
+
+    printf("\n");
+  }
 
   printf("Calcolare phi(n) <= %d? [Y/n] ", n);
   scanf("%c", &all_phi);
